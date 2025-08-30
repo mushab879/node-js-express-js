0d5d93d95bed6986c6c89eab690078eea8e56d48
mushab879 <mmushab37@gmail.com>
2025-08-30 18:18:59 +0530

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
-
-// Asynchronous (Non-blocking)
 fs.readFile('file.txt', 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data); // This runs later when file is ready
+  console.log(data); 
 });
 
 console.log("This runs immediately, without waiting for file");
